093303f72c78d1a5061a9931b343763f1feebb93
David <Rigby335@hotmail.com>
2022-05-09 14:29:44 -0700

    testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,19 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { tools } from '../tools.js';
+import { renderTools } from '../utils.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test tool render function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="tool"><img src="./assets/shovel.jpg" alt=""><h1>Spades</h1><p>The Spade has a digging factor of 7 and a durability factor of 9</p><h2>Perks</h2><ul><li>Easy to use</li><li>Versatile</li><li>Decent at killing zombies</li></ul></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderTools(tools[0]);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
---
+++ utils.js
@@ -0,0 +1,6 @@
+export function renderTools(tool) {
+    const div = document.createElement('div');
+    div.classList.add('tool');
+
+    return div;
+}
